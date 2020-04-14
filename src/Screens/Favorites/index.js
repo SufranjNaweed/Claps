7cd06ce3e88ae384584e3e327936d979a8226821
SufranjNaweed <sufranj.naweed@gmail.com>
2020-04-14 11:38:16 +0200

    add : Favorite Screen

--- src/Screens/Favorites/index.js
+++ src/Screens/Favorites/index.js
@@ -1,35 +1,58 @@
-import React, { useContext} from 'react';
+import React, { useContext, useState } from 'react';
 import {
     Text,
     View,
     FlatList
   } from 'react-native';
+import {ButtonGroup} from 'react-native-elements';
 
 import {FavoriteContext} from '../../Contexts/Favorites';
 import MainLayout from '../../Layouts/MainLayout';
 import ClapsCard from '../../Components/ClapsCard';
 
 export default function Favorites() {
-    const {favoritesGifs} = useContext(FavoriteContext);
+    const {favoritesGifs, favoritesStickers} = useContext(FavoriteContext);
 
-    console.log("------------- Favorites START -------------");
+    const [selectIndex, setSelectIndex] = useState(0);
+    const buttons = ['Gifs', 'Stickers'];
 
-    console.log("favoriteGifs => ", favoritesGifs);
-    const len = favoritesGifs.length
-    console.log("favoriteGifs length => ", len)
+    const lenFavGifs = favoritesGifs.length;
+    const lenFavStickers = favoritesStickers.length;
 
-    console.log("------------- Favorites END -------------");
+    const updateIndex = (selectIndex) => {
+        setSelectIndex(selectIndex);
+    }
+
     return (
         <MainLayout screenTitle='Favorites'>
-            <Text>number of favoritesGifs : {len} </Text>
-
-            <View style={{flex: 1}}>
+            <ButtonGroup 
+                onPress={updateIndex}
+                selectedIndex={selectIndex} 
+                buttons={buttons} 
+                containerStyle={{height : 40, borderRadius: 8}}
+            />
+            {
+                selectIndex === 0 ?
+                (
+                    <View style={{flex: 1}}>
+                        <Text style={{textAlign: "center", fontSize: 20}}>Number of favorites gifs : {lenFavGifs} </Text>
                         <FlatList 
                             data={favoritesGifs} 
                             keyExtractor={favoritesGif => favoritesGif.id} 
-                            renderItem={(favoritesGif) => <ClapsCard data={favoritesGif} typeOfMedia="gifs" />}
+                            renderItem={(favoritesGif) => <ClapsCard data={favoritesGif} typeOfMedia="gifs" isFav={true} />}
                         />
                     </View>
+                ) : (
+                    <View style={{flex: 1}}>
+                        <Text style={{textAlign: "center", fontSize: 20}}>Number of favorites stickers : {lenFavStickers} </Text>
+                        <FlatList 
+                            data={favoritesStickers} 
+                            keyExtractor={favoritesSticker => favoritesSticker.id} 
+                            renderItem={(favoritesSticker) => <ClapsCard data={favoritesSticker} typeOfMedia="gifs" isFav={true} />}
+                        />
+                    </View>
+                )
+            }
         </MainLayout>
     );
 }
