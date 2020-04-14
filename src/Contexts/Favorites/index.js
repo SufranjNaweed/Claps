d50a180452306d466bf84f856ea5aa16c987dc5e
SufranjNaweed <sufranj.naweed@gmail.com>
2020-04-14 10:22:41 +0200

    fixing toggleToFfavorite

--- src/Contexts/Favorites/index.js
+++ src/Contexts/Favorites/index.js
@@ -1,4 +1,5 @@
 import React, {createContext, useState, useEffect} from 'react';
+import Toast from 'react-native-simple-toast';
 
 export const FavoriteContext = createContext();
 
@@ -10,18 +11,30 @@ const FavoriteContextProvider = (props) => {
 
     const toggleToFavorites = (typeOfMedia, data) => {
         const dataItem = data.item;
-        console.log('---------------------------');
-
-        console.log("dataItem.id -> ", dataItem.id);
-        setFavoriteGifs([dataItem, ...favoritesGifs]);
-        console.log("toggleToFavorites -> favoritesGifs", favoritesGifs)
-        
-       
         if(typeOfMedia === "gifs"){
-
+            const findMyMedia = favoritesGifs.filter((favGif) => favGif.id === dataItem.id)
+            if (findMyMedia.length === 0){
+                setFavoriteGifs([dataItem, ...favoritesGifs]); 
+                Toast.show('📼 This gif has been add to your favorite ! 🚀', Toast.LONG, Toast.BOTTOM);
+            }
+            if (findMyMedia.length > 0){
+                const newFavoritesGifs = favoritesGifs.filter((favGif) => favGif.id !== dataItem.id);
+                setFavoriteGifs([...newFavoritesGifs]);
+                Toast.show('📼 This gif has been remove from your favorite 😥', Toast.SHORT, Toast.BOTTOM);
+            }   
         }
-
-        console.log('---------------------------');
+        if(typeOfMedia === 'stickers'){
+            const findMyMedia = favoritesStickers.filter((favSticker) => favSticker.id === dataItem.id)
+            if (findMyMedia.length === 0){
+                setFavoriteStickers([dataItem, ...favoritesStickers]);
+                Toast.show('🖼️ This sticker has been add to your favorite ! 🚀', Toast.LONG, Toast.BOTTOM);
+            }
+            if (findMyMedia.length > 0){
+                const newFavoritesStickers = favoritesStickers.filter((favSticker) => favSticker.id !== dataItem.id);
+                setFavoriteStickers([...newFavoritesStickers]);
+                Toast.show('🖼️ This sticker has been remove from your favorite 😥', Toast.SHORT, Toast.BOTTOM);
+            }
+        }
     }
 
     return (
