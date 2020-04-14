de02a61fb51d311685265718b58f699b776ad7fc
SufranjNaweed <sufranj.naweed@gmail.com>
2020-04-14 11:40:53 +0200

    update & add : Toast & Clipboard to ClapsCard

--- src/Components/ClapsCard/index.js
+++ src/Components/ClapsCard/index.js
@@ -1,19 +1,33 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import {Card, Button, Icon} from 'react-native-elements'
+import {useClipboard} from '@react-native-community/clipboard';
+import Toast from 'react-native-simple-toast';
 
 import {FavoriteContext} from '../../Contexts/Favorites';
 
-const  ClapsCard = ({data, typeOfMedia}) => {
+const  ClapsCard = ({data, typeOfMedia, isFav}) => {
     const dataItem = data.item;
     const image = dataItem.images.downsized.url;
     const text = dataItem.title;
+    const originalUrl = dataItem.images.original.url;
 
     const { toggleToFavorites } = useContext(FavoriteContext);
+    const [isFavorite, setIsFavorite] = useState(); 
+    const [clipboardData, setSring] = useClipboard()
+    
+    useEffect(() => {
+        setIsFavorite(isFav)
+    }, [])
 
     const actionToggleToFavorite = () => {
-        console.log('actionToggleToFavorite');
         toggleToFavorites(typeOfMedia, data);
+        setIsFavorite(!isFavorite);
+    }
+
+    const copyMediaUrl = () => {
+        setSring(originalUrl);
+        Toast.show('The links has been copy in your paste bin', Toast.LONG, Toast.BOTTOM);
     }
 
     return(
@@ -27,16 +41,16 @@ const  ClapsCard = ({data, typeOfMedia}) => {
             </Text>
             <View style={styles.buttonContainer}>
                 <Button
-                    icon={<Icon name='heart' type='material-community' color='#ffffff' />}
+                    icon={<Icon name={isFavorite ? 'heart' : 'heart-outline'} type='material-community' color='#ffffff' />}
                     buttonStyle={[styles.actionButton, {backgroundColor: "red"}]}
                     title='' 
-                    onPress={() => actionToggleToFavorite()}
+                    onPress={actionToggleToFavorite}
                 />
                 <Button
                     icon={<Icon name='link-variant' type='material-community' color='#ffffff' />}
                     buttonStyle={styles.actionButton}
                     title='' 
-                    
+                    onPress={copyMediaUrl}
                 />
             </View>
         </Card>
@@ -63,7 +77,11 @@ const styles = StyleSheet.create({
         borderRadius: 8,
         marginLeft: 0,
         marginRight: 0,
-        marginBottom: 0
+        marginBottom: 0,
+        paddingTop : 10,
+        paddingBottom : 10,
+        paddingLeft : 50,
+        paddingRight : 50
     }
 });
 
