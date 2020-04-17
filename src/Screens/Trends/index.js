47dc37054d3e28e8ba69236c81f1eb42ee56e9cc
SufranjNaweed <sufranj.naweed@gmail.com>
2020-04-17 09:36:31 +0200

    delete : Stickers mock & update : Trends Screen

--- src/Screens/Trends/index.js
+++ src/Screens/Trends/index.js
@@ -1,27 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import {FlatList, View} from 'react-native';
-import {ButtonGroup} from 'react-native-elements';
 import MainLayout from '../../Layouts/MainLayout';
 import ClapsCard from '../../Components/ClapsCard';
-
-import AsyncStorage from '@react-native-community/async-storage';
 import axios from 'axios';
 
-import {gifsMock} from '../../Configs/Mocks/Gifs';
-import {stickersMock} from '../../Configs/Mocks/Stickers';
 import {API_URL} from '../../Configs/API/index';
 
 export default function Trends() {
     
-    const [gifs, setGifs] = useState(gifsMock);
-    const [stickers, setStickers] = useState(stickersMock);
-    const [selectIndex, setSelectIndex] = useState(0);
-
-    const buttons = ['Gifs', 'Stickers'];
+    const [gifs, setGifs] = useState([]);
 
     useEffect(() => {
-        // uncomment when the app is over
-        // getGifs();
+         getGifs();
     },[]);
     
     const getGifs = async () => {
@@ -40,43 +30,15 @@ export default function Trends() {
         }
     }
 
-    const getStickers = async () => {
-        
-    }
-
-    const updateIndex = (selectIndex) => {
-        setSelectIndex(selectIndex);
-        console.log(selectIndex);
-    }
-    
-
     return (
         <MainLayout screenTitle='Trends'>
-            <ButtonGroup 
-                onPress={updateIndex}
-                selectedIndex={selectIndex} 
-                buttons={buttons} 
-                containerStyle={{height : 40, borderRadius: 8}}
-            />
-            {
-                selectIndex === 0 ? (
-                    <View style={{flex: 1}}>
-                        <FlatList 
-                            data={gifs} 
-                            keyExtractor={gif => gif.id} 
-                            renderItem={(gif) => <ClapsCard data={gif} typeOfMedia="gifs" isFav={false} />}
-                        />
-                    </View>
-                ) : (
-                    <View style={{flex: 1}}>
-                        <FlatList 
-                            data={stickers} 
-                            keyExtractor={sticker => sticker.id} 
-                            renderItem={(sticker) => <ClapsCard data={sticker} typeOfMedia="stickers" isFav={false} />}
-                        />
-                    </View>
-                )
-            }
+            <View style={{flex: 1}}>
+                <FlatList 
+                    data={gifs} 
+                    keyExtractor={gif => gif.id} 
+                    renderItem={(gif) => <ClapsCard data={gif} typeOfMedia="gifs" isFav={false} />}
+                />
+            </View>
         </MainLayout>
     );
 }
